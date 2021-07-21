b7beab58f2a28d449eda6573919d2939c5a49d24
harishwar1201 <87687891+harishwar1201@users.noreply.github.com>
2021-07-21 18:23:53 +0530

    Add files via upload

---
+++ Day1/Exercise3/solution3.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type Employee interface{
+	GetSalary() int
+}
+type FulltimeEmployee struct {
+	name string
+	Duration int
+}
+type Contractor struct {
+	name string
+	Duration int
+}
+type Freelancer struct {
+	name string
+	Duration int
+}
+func (fulltimeEmp FulltimeEmployee) GetSalary() int {
+	return fulltimeEmp.Duration*500
+}
+func (contr Contractor) GetSalary()int {
+	return contr.Duration*100
+}
+func (flancer Freelancer) GetSalary() int {
+	return flancer.Duration*10
+}
+
+func main(){
+	employee1:=FulltimeEmployee{"Ramesh",28}
+	employee2:=Contractor{"Rajesh",28}
+	employee3:=Freelancer{"Suresh",20}
+	fmt.Println("Total Salary of employee1 is ", employee1.GetSalary())
+	fmt.Println("Total Salary of employee2 is ", employee2.GetSalary())
+	fmt.Println("Total Salary of employee3 is ", employee3.GetSalary())
+}
+
+
