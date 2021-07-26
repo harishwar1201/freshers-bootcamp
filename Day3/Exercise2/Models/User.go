3ccba7d34bfa1644041a1c84307ab07a6697821e
harishwar1201 <87687891+harishwar1201@users.noreply.github.com>
2021-07-26 12:46:38 +0530

    Student record database using http calls first commit

---
+++ Day3/Exercise2/Models/User.go
@@ -0,0 +1,38 @@
+package Models
+import (
+	"fmt"
+	"github.com/freshers-bootcamp/Day3/Exercise2/Config"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func GetAllUsers(user *[]User) (err error) {
+	if err = Config.DB.Find(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func CreateUser(user *User) (err error) {
+	if err = Config.DB.Create(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetUserByID(user *User, id string) (err error) {
+	if err = Config.DB.Where("id = ?", id).First(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func UpdateUser(user *User, id string) (err error) {
+	fmt.Println(user)
+	Config.DB.Save(user)
+	return nil
+}
+//DeleteUser ... Delete user
+func DeleteUser(user *User, id string) (err error) {
+	Config.DB.Where("id = ?", id).Delete(user)
+	return nil
+}
